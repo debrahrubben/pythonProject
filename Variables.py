b38da7e64c09ed7b09f9f9c06c00c9ad9da92bc7
Rubben Debrah <64004519+debrahrubben@users.noreply.github.com>
2023-12-22 21:49:47 -0800

    Update Variables.py

--- Variables.py
+++ Variables.py
@@ -1,5 +1,8 @@
 First_Name = "Rubben"
 Last_Name = "Debrah"
 Age = 23
-Full_Name = First_Name + Last_Name
-print (Full_Name+" "+ "is"+"  "+"years Old" )
+Full_Name = First_Name + " " + Last_Name
+Height = 76.4
+Female = False
+
+print(Full_Name + " is " + str(Age) + " years old. He is " + str(Height) + " cm. Is he a female? " + str(Female))
