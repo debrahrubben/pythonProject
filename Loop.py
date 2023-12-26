5e719e879921f3976d9018f2b4271f3c1e41b7b4
Rubben Debrah <64004519+debrahrubben@users.noreply.github.com>
2023-12-26 15:06:11 -0800

    loop

---
+++ Loop.py
@@ -0,0 +1,17 @@
+import time
+
+# while loop
+name = ''
+# while len(name) == 0:
+#     name = input("Enter your name: ")
+#     print("Hello "+name)
+
+    # for loop
+
+# for i in range(50,100,2):
+#         print(i+1)
+
+for seconds in range(10,0,-1):
+    print(seconds)
+    time.sleep(1)
+    print('Happy New Year')
