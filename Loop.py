c88779825bd22aa998483e0198e0761949ef17ed
Rubben Debrah <64004519+debrahrubben@users.noreply.github.com>
2023-12-26 21:10:29 -0800

    today

--- Loop.py
+++ Loop.py
@@ -11,7 +11,24 @@ name = ''
 # for i in range(50,100,2):
 #         print(i+1)
 
-for seconds in range(10,0,-1):
-    print(seconds)
-    time.sleep(1)
-    print('Happy New Year')
+# for seconds in range(10,0,-1):
+#     print(seconds)
+#     time.sleep(1)
+#     print('Happy New Year')
+
+    # nested loop
+
+# rows = int(input('How many rows?: '))
+# columns = int(input('How many columns?: '))
+# symbol = input('Enter a symbol to use: ')
+# for i in range(rows):
+#     for j in range(columns):
+#         print(symbol, end='')
+#         print()
+
+# loop control statement
+while True:
+name = input("Enter your name: ")
+if name != "":
+    break
+
