011de94797de3acc77c64b622c645106574a6f35
Rubben Debrah <64004519+debrahrubben@users.noreply.github.com>
2023-12-30 15:42:54 -0800

    hmm

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+ # dictionary = a changeable, unordered collection of unique key:value pairs
+#fast because they use  hashing, allowing us to access a value quickly
+
+capitals = {'USA':'WASHINGTON DC',
+              'INDIA':'NEW DELHI',
+              'CHINA':'BEIJIN',
+              'RUSSIA':'MOSCOW'}
+for i in capitals:
+    print(capitals.items())
--- set.py
+++ set.py
@@ -1,3 +1,4 @@
 # set = collection which is unordered. No duplicate values
 
- 
+# utensil = {'fork', 'spoon', 'knife'}
+# dishes = {'bowl'}......
