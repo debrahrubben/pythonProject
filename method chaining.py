f71e531378ac1eae8fa3e147c935d80d48f6d21b
Rubben Debrah <64004519+debrahrubben@users.noreply.github.com>
2024-01-02 18:14:40 -0800

    method

---
+++ method chaining.py
@@ -0,0 +1,12 @@
+class Car:
+    def turn_on(self):
+        print('You start the engine')
+        return self
+    def drive(self):
+        print('You drive the car')
+        return self
+    def brake(self):
+        print('you step on the brakes')
+        return self
+
+    Car.
